19e6bf725cff269cc167ebce3db1598e55f8d231
nabilasaf <nabilasafitri861@gmail.com>
2026-01-02 17:22:46 +0700

    menambahkan ContainerApp.kt

---
+++ app/src/main/java/com/example/prak14/repositori/ContainerApp.kt
@@ -0,0 +1,25 @@
+package com.example.prak14.repositori
+
+import android.app.Application
+import com.example.myfirebase.repositori.FirebaseRepositorySiswa
+import com.example.myfirebase.repositori.RepositorySiswa
+import com.google.firebase.Firebase
+
+interface ContainerApp{
+    val repositorySiswa: RepositorySiswa
+}
+
+class DefaultContainerApp : ContainerApp{
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
+
+class AplikasiDataSiswa : Application(){
+    lateinit var container: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
+
+    }
+}
