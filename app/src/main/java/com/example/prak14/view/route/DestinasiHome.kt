7cb4d21f99a1256bd5f1d62b132a45068e26403a
nabilasaf <nabilasafitri861@gmail.com>
2026-01-02 17:24:21 +0700

    menambahkan DestinasiHome.kt

---
+++ app/src/main/java/com/example/prak14/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.prak14.view.route
+
+import com.example.prak14.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
