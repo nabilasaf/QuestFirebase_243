146befdb4a2ec9c1e161d2af92bfbfe141e1a173
nabilasaf <nabilasafitri861@gmail.com>
2026-01-02 17:26:53 +0700

    menambahkan main activity

--- app/src/main/java/com/example/prak14/MainActivity.kt
+++ app/src/main/java/com/example/prak14/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.prak14.ui.theme.PRAK14Theme
+import com.example.prak14.view.controllNavigasi.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PRAK14Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
