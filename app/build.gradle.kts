7c30e41b0e6bad38ad6af7f52498418c9354bbba
nabilasaf <nabilasafitri861@gmail.com>
2026-01-02 17:22:33 +0700

    menambahkan data di build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,10 @@ dependencies {
     //viewmodel
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui)
 
+    implementation("androidx.compose.material:material-icons-extended")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
