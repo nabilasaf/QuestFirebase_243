d20a8f34149a2709a5a67f92baefde2b941e4922
nabilasaf <nabilasafitri861@gmail.com>
2026-01-02 17:23:14 +0700

    menambahkan DestinasiDetail.kt

---
+++ app/src/main/java/com/example/prak14/view/route/DestinasiDetail.kt
@@ -0,0 +1,10 @@
+package com.example.prak14.view.route
+
+import com.example.prak14.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
