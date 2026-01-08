3256e70dcb08fba12573a5ad1952056885576218
nabilasaf <nabilasafitri861@gmail.com>
2026-01-08 09:08:04 +0700

    Menambahkan navigasi edit  untuk data siswa

--- app/src/main/java/com/example/prak14/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/prak14/view/controllNavigasi/PetaNavigasi.kt
@@ -71,5 +71,22 @@ fun HostNavigasi(
             )
         }
 
+        composable(
+            DestinasiEdit.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiEdit.itemIdArg) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            EditSiswaScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route)
+                },
+                onNavigateUp = {
+                    navController.navigateUp()
+                }
+            )
+        }
     }
 }
