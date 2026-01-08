b230d12d9bb6fad7cb86bfb7f77c78c48d88de74
nabilasaf <nabilasafitri861@gmail.com>
2026-01-08 08:28:15 +0700

    Menambahkan data build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.androidx.ui)
 
     implementation("androidx.compose.material:material-icons-extended")
+    implementation(libs.androidx.lifecycle.viewmodel.savedstate)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
