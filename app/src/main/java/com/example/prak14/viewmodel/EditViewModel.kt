9037d76ac53300671ff2a1c542f569d9b52d78bf
nabilasaf <nabilasafitri861@gmail.com>
2026-01-08 08:05:55 +0700

    Penambahan fitur pengelolaan data siswa pada EditViewModel

---
+++ app/src/main/java/com/example/prak14/viewmodel/EditViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.prak14.viewmodel
+
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myfirebase.repository.RepositorySiswa
+import com.example.prak14.modeldata.DetailSiswa
+import com.example.prak14.modeldata.UIStateSiswa
+import com.example.prak14.modeldata.toDataSiswa
+import com.example.prak14.modeldata.toUiStateSiswa
+import com.example.prak14.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
+
+
+}
